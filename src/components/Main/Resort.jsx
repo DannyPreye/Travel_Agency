b58193828e417d95ecf6a88b260f4578b9fbc5ab
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-06-25 13:25:16 +0100

    editted

--- src/components/Main/Resort.jsx
+++ src/components/Main/Resort.jsx
@@ -18,53 +18,11 @@ const Resort = () => {
             The best resorts to chill and relax
           </h1>
           <ResortCard img={nature} />
-          {/* <div className="w-[90%] lg:w-[484px] h-[711px] rounded-[20px]  relative overflow-hidden">
-            <img
-              src={nature}
-              alt="nature resort"
-              className=" w-full h-full  absolute top-0 left-0 z-[1000]"
-            />
-          </div> */}
           <ResortCard img={island} />
-          {/* <div className="w-[90%] lg:w-[484px] h-[711px] rounded-[20px]  relative overflow-hidden">
-            <img
-              src={island}
-              alt="nature resort"
-              className=" w-full h-full  absolute top-0 left-0 z-[1000]"
-            />
-          </div> */}
         </div>
         <div className="flex flex-col items-center gap-[32px]">
           <ResortCard img={winter} />
-          {/* <div className="w-[90%] lg:w-[484px] h-[711px] rounded-[20px]  relative overflow-hidden">
-            <div className="absolute top-0 left-0 w-full h-full z-[100] winter"></div>
-            <img
-              src={winter}
-              alt="nature resort"
-              className=" w-full h-full  absolute top-0 left-0 "
-            />
-            <div className="z-[1000] text-[#fff] absolute bottom-0 left-[15%] mb-[65px] w-[27px] h-[170px] ">
-              <div>
-                <h1 className="w-[355px] h-fit font-[600] text-[28px]  leading-[42px] mb-[16px]">
-                  Winter landscape Chalet
-                </h1>
-                <p className="w-[98px] h-[27px] font-[500] text-[18px] leading-[27px]">
-                  Cambodia
-                </p>
-              </div>
-              <button className="w-[355px] h-[64px] mt-[28px] text-[16px] leading-[24px] font-[600] bg-[#3E86F5] rounded-[10px] grid place-items-center">
-                Book Now
-              </button>
-            </div>
-          </div> */}
           <ResortCard img={sk} />
-          {/* <div className="w-[90%] lg:w-[484px] h-[711px] rounded-[20px]   relative overflow-hidden">
-            <img
-              src={sk}
-              alt="nature resort"
-              className=" w-full h-full  absolute top-0 left-0 z-[1000]"
-            />
-          </div> */}
         </div>
       </div>
     </div>
